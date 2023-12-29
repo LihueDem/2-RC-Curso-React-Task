f429da9e3389580eb254eee2036a4e96c71a6c01
Lihue <lihue.demelchiorre@gmail.com>
2023-12-29 17:40:27 -0300

    Terminamos la elevacion de estado para addDataCard y deleteCard

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { TaskCard } from "./TaskCard";
 import { TaskAdd } from "./TaskAdd";
-import { dataCard } from "../../data/Task";
+import { dataCard } from "../../data/Task.js";
 
 export const Task = () => {
-  const onClickTaskAdd = (newValue) => {
-    //console.log(valueTask);
-    console.log(newValue);
+  const [addDataCard, setAddDataCard] = useState(dataCard);
+
+  const onClickTaskAdd = (value) => {
+    //console.log(value);
+
+    const newTaskObj = {
+      //id: addDataCard.at(- 1).id + 1, nueva forma de conseguir el indece del array
+      id: addDataCard[addDataCard.length - 1].id + 1,
+      taskTitle: value,
+    };
+
+    setAddDataCard([...addDataCard, newTaskObj]);
+
+    console.log(newTaskObj);
+  };
+
+  const onClickDelete = (id) => {
+    const newArrCard = dataCard.filter((item) => item.id != id);
+    setAddDataCard(newArrCard);
   };
 
   return (
@@ -14,13 +30,20 @@ export const Task = () => {
       <div className="container py-5" id="featured-3">
         <h2 className="pb-2 border-bottom text-white">MIS TAREAS</h2>
         <div className="row py-5">
-          {dataCard.map((item) => (
-            <TaskCard key={item.id} dataCardItem={item} />
+          {addDataCard.map((item) => (
+            <TaskCard
+              key={item.id}
+              dataCardItem={item}
+              deleteCard={(value) => onClickDelete(value)}
+            />
           ))}
         </div>
-        {/*Callback */}
+
         <TaskAdd onClickPadre={(value) => onClickTaskAdd(value)} />
       </div>
     </>
   );
 };
+
+//Callback
+//<TaskAdd onClickPadre={(value) => onClickTaskAdd
